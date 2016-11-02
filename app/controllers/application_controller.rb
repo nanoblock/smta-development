70bfe9421d5551fd987c1855b0e65189e00da3f7
nanoblock <yongseok.jang@grep.services>
2016-11-02 10:42:56 +0900

    Added getter browser type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :browser_type
+
+  def browser_type
+    @browser = set_browser_type
+  end
+
+  private
+  def set_browser_type
+    return request.variant = :phone if browser.device.mobile?
+    return request.variant = :tablet if browser.device.tablet?
+    return request.variant = :desktop if !browser.device.mobile? && !browser.device.tablet?
+  end
 end
